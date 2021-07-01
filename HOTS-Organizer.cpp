7589954269f7cbcbb471fb63839e189b19150646
NotCreative001 <55113844+NotCreative001@users.noreply.github.com>
2021-07-01 10:42:13 -0400

    added namespace because typing std is annoying
    needing to type std for all input and output operations as well as before the variable type of string is really annoying so ill just add the namespace

--- HOTS-Organizer.cpp
+++ HOTS-Organizer.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+using namespace std;
 
 enum role {Tank, Bruiser, Ranged_Assassin, Melee_Assassin, Healer, Support}; //class of hero
 enum universe {Warcraft, StarCraft, Diablo, Overwatch, Nexus}; //heros original game
@@ -7,18 +10,22 @@ enum universe {Warcraft, StarCraft, Diablo, Overwatch, Nexus}; //heros original 
 //contains aspects of the heros
 class hero {
     public:
+        string heroName;
         role heroRole;
         universe heroUni;
 };
 
 int main() {
-    std::ifstream heroList;
+    ifstream heroList;
     heroList.open("heroList.txt");
     if(!heroList.is_open()) {
-        std::cout << "heroList.txt failed to open please check your file";
+        cout << "heroList.txt failed to open please check your file";
         return -1;
     }
-    std::cout << "opened file correctly";
+    cout << "opened file correctly";
+
+
+
     heroList.close();
     return 0;
 }
